3537a5e4f8d89220aded61981b5f46084e19f941
Mário Pinto <mario.pinto@ccg.pt>
2013-05-09 17:39:16 +0100

    Timer Realocation (CV)

--- ToolBox/ToolBox.h
+++ ToolBox/ToolBox.h
@@ -1,30 +1,7 @@
 #ifndef _TOOLBOX
 #define _TOOLBOX
 
-#include <string>
-
 namespace ToolBox{
-	class Timer{
-		public:
-			Timer();
-			~Timer();
-
-			void start_timer();
-
-			double pause_timer();
-			double stop_timer();
-			double restart_timer();
-
-			double time_elapsed_seconds();
-			double time_elapsed_milliseconds();
-
-			std::string* time_elapsed_seconds_str();
-			std::string* time_elapsed_milliseconds_str();
-
-		private:
-			int asd;
-	};
-
 	void coiso();
 }
 
--- ToolBoxCV/ToolBoxCV.h
+++ ToolBoxCV/ToolBoxCV.h
@@ -4,6 +4,27 @@
 #include <opencv2\opencv.hpp>
 
 namespace ToolBoxCV{
+	class Timer{
+		public:
+			Timer();
+			~Timer();
+
+			void start_timer();
+
+			double pause_timer();
+			double stop_timer();
+			double restart_timer();
+
+			double time_elapsed_seconds();
+			double time_elapsed_milliseconds();
+
+			std::string* time_elapsed_seconds_str();
+			std::string* time_elapsed_milliseconds_str();
+
+		private:
+			int asd;
+	};
+
 	bool in_range(cv::Point* point, int width = 640, int height = 480);
 }
 
